15d9e834d788f15ffe55ac63e107413600d62f83
Rick Hennessey <rhennessey3@gmail.com>
2025-03-17 18:36:29 -0400

    Update middlewares.js
    added cors

--- strapi-main/config/middlewares.js
+++ strapi-main/config/middlewares.js
@@ -1,8 +1,28 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['https://cozy-case-studies.vercel.app', 'http://localhost:3000', 'http://localhost:5173']
+    }
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
